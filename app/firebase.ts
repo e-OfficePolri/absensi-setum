957c5ef6c8b47aace95db015a05f4b93c7ea8af1
e-OfficePolri <irfannrmdnn@gmail.com>
2026-08-17 02:03:58 +0700

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmu-VOUrhgEZxoFTC0rgVN15vKwThezyA",
+  apiKey: "AIzaSyCmu-V0UrhgEZxoFTC0rgVN15vKwThezyA",
   authDomain: "absensi-setum.firebaseapp.com",
   projectId: "absensi-setum",
   storageBucket: "absensi-setum.firebasestorage.app",
   messagingSenderId: "130947937553",
-  appId: "1:130947937553:web:..."
+  appId: "1:130947937553:web:3421f311b413e2197548b0"
 };
 
 const app = initializeApp(firebaseConfig);
